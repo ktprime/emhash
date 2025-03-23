3f089413c1ac11653b6948f909b64045cdb79af0
Abdelrahman Farid <abdelrahman.farid@gmail.com>
2025-03-23 23:59:00 +0200

    hash_table8.hpp: Make hash_function() return const HashT&

--- hash_table8.hpp
+++ hash_table8.hpp
@@ -416,7 +416,7 @@ public:
     size_type bucket_count() const { return _num_buckets; }
     float load_factor() const { return static_cast<float>(_num_filled) / ((float)_mask + 1.0f); }
 
-    HashT& hash_function() const { return _hasher; }
+    const HashT& hash_function() const { return _hasher; }
     EqT& key_eq() const { return _eq; }
 
     void max_load_factor(float mlf)
